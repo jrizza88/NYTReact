806a63cf771aa03ad65b4a874382b6cc8de129b3
jrizza88 <jetorres88@gmail.com>
2016-09-20 00:13:26 -0400

    react is working

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Include the Main React Dependencies
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Include the Main Component
+//var Main = require('./app.app.js')
+
+// This code here allows us to render our main component (in this case "Main")
+ReactDOM.render(
+<p>Hello World!</p>, 
+  document.getElementById('app')
+)
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+//Require Schemas
+
+
+// Create Instance of Express
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+// -------------------------------------------------
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
